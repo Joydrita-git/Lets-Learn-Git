b02c80765f47280ef92a344ca42b5d8e068981e6
Joydrita Datta <jjoydritadatta@salesforce.com>
2025-05-16 21:57:56 +0530

    committed

---
+++ Diff.java
@@ -0,0 +1,19 @@
+public class Multiply {
+    // Method to calculate product of two numbers
+    public static int calculateProduct(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static void main(String[] args) {
+        // Example numbers
+        int number1 = 5;
+        int number2 = 6;
+        
+        // Calculate product
+        int product = calculateProduct(number1, number2);
+        
+        // Print the result
+        System.out.println("Product of " + number1 + " and " + number2 + " is: " + product);
+    }
+}
+// joy 
